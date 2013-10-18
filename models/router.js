5b92fbcf0cf06cc5e18510ee2d594c7e46290125
André Gaul <gaul@web-yard.de>
2013-10-18 20:23:01 +0200

    add dependency: libremap-common

--- models/router.js
+++ models/router.js
@@ -2,5 +2,6 @@ var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
   urlRoot: '/api/router/',
-  idAttribute: '_id'
+  idAttribute: '_id',
+  validate: require('libremap-common').router_validate
 });
